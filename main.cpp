0fc14714557de1e54f464bd9cf0cc7e1a8af7d5e
Mike <michalbaz@poczta.onet.pl>
2022-04-24 01:16:02 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "serial.h"
+
+class serial_test {
+public:
+	static void main(void){
+
+	}
+};
+
+int main(void){
+	serial_test::main();
+}
---
+++ serial.h
@@ -0,0 +1,4 @@
+#ifndef serial_h_
+#define serial_h_
+
+#endif
